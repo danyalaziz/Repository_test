6020137355031dc758bbef4ac348990fbfde2a00
danyalaziz <44175446+danyalaziz@users.noreply.github.com>
2018-10-18 14:21:48 -0600

    Hey, I added this great script with an extra line of comment.

---
+++ NewRScript.R
@@ -0,0 +1,11 @@
+#test script for GitHub
+myfunction <- function(x,y){
+  if(x>y){
+    print("x is greater than y")
+  }else{
+    print("y is greater than or equal to x")
+  }
+}
+#I'm changing danyal's script
+
+z<-myfunction(x=10,y=5)
